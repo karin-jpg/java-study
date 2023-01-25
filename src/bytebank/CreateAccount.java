6d31a935218ae2eb2b29a2b3e4b7dda70d580dd5
Karín Morais <andradkarin@gmail.com>
2023-01-25 15:39:30 -0300

    Add more messages and tests

--- src/bytebank/CreateAccount.java
+++ src/bytebank/CreateAccount.java
@@ -14,17 +14,23 @@ public class CreateAccount {
 		
 		Client client = new Client();
 				
-		client.setName("Karn Morais");
+		client.setName("Karin Morais");
 		client.setCpf("265.458.456-06");
 		client.setProfession("Developer");
 		
 		account.setClient(client);
 		
+		System.out.println("The account owner is " + account.getClient().getName());
+		System.out.println("The account owner profession is " + account.getClient().getProfession());
 		System.out.println("Your account number is "+ account.getNumber());
 		System.out.println("Your account balance is "+ String.format(Locale.GERMAN, "%,.2f", account.getBalance()));
 		System.out.println();
 		account.withdraw(25.65);
+		
+		account.getClient().setProfession("Programmer");
 
+		System.out.println("The account owner is " + account.getClient().getName());
+		System.out.println("The account owner profession is " + account.getClient().getProfession());
 		System.out.println("Your account number is "+ account.getNumber());
 		System.out.println("Your new balance is "+ String.format(Locale.GERMAN, "%,.2f", account.getBalance()));
 		System.out.println();
